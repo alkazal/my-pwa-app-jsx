1740c6c85059145d34ad3eb26d64496bb40a5694
mrizal <mrizal@scs.my>
2025-11-18 09:11:28 +0800

    Add  greeting caps

--- src/App.jsx
+++ src/App.jsx
@@ -18,13 +18,10 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <h3>Welcome Razi RND to PWA!</h3>
+        <h2>WELCOME TO PWA!</h2>
         <button onClick={() => setCount((count) => count + 1)}>
           Click to count and it is counting {count} 
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
